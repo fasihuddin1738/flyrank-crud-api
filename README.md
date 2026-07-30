099c3390723498ec4afd7356fd2f13585dd8e753
Fasih Lodhi <lodhifasih0@gmail.com>
2026-07-30 23:24:32 +0500

    Stage 3: POST endpoints + README file added

---
+++ README.md
@@ -0,0 +1,24 @@
+# Flyrank Task API
+
+A CRUD API for managing a to-do list, built with Python and FastAPI. 
+
+## How to Install & Run
+To start the server on your local machine, activate the virtual environment and run the following command:
+`uvicorn main:app --reload`
+
+## Endpoints
+
+| Operation | HTTP Method | Endpoint | Description |
+| :--- | :--- | :--- | :--- |
+| **API Info** | GET | `/` | Returns API name and version |
+| **Health Check** | GET | `/health` | Checks if the server is alive |
+| **Read All** | GET | `/tasks` | Returns a list of all tasks |
+| **Read One** | GET | `/tasks/{id}` | Returns a single task by ID |
+| **Create** | POST | `/tasks` | Creates a new task |
+| **Update** | PUT | `/tasks/{id}` | *Pending Stage 4* |
+| **Delete** | DELETE | `/tasks/{id}` | *Pending Stage 4* |
+
+## Example Request & Response
+
+```text
+![alt text](/Screenshots/Stage 3.png)
--- main.py
+++ main.py
@@ -2,8 +2,13 @@
 from fastapi import FastAPI
 # pyrefly: ignore [missing-import]
 from fastapi.responses import JSONResponse
+# pyrefly: ignore [missing-import]
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class TaskCreate(BaseModel):
+    title: str | None = None 
 
 tasks = [
     {"id": 1, "title": "Buy milk", "done": False},
@@ -23,10 +28,30 @@ def health():
 def get_all_tasks():
     return tasks 
 
-@app.get("/tasks/{id}")
+@app.get("/tasks/{id}", responses={404: {"description": f"Task {id} not found"}})
 def get_task(id: int):
     for task in tasks:
         if task["id"] == id:
             return task
     
     return JSONResponse(status_code=404, content={"error": f"Task {id} not found"})
+
+@app.post("/tasks", responses={400: {"description": "Invalid input - Title is missing or empty"}})
+def create_task(task_in: TaskCreate):
+    if not task_in.title or not task_in.title.strip():
+        return JSONResponse(status_code=400, content={"error": "Title is missing or empty"})
+    
+    if len(tasks) > 0:
+        next_id = max(task["id"] for task in tasks) + 1
+    else:
+        next_id = 1
+    
+    new_task = {
+        "id": next_id,
+        "title": task_in.title.strip(),
+        "done": False
+    }
+
+    tasks.append(new_task)
+
+    return JSONResponse(status_code=201, content=new_task)
